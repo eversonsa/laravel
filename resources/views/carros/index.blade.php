888f9b310493dfb62d887d19b5e3f67c89fbc2af
everson <everson@everson-PC>
2016-04-25 14:47:50 -0300

    comit atualizado

--- resources/views/carros/index.blade.php
+++ resources/views/carros/index.blade.php
@@ -1,13 +1,10 @@
 <h1>Bem vindo a listagem</h1>
 {{-- Lista os carros --}}
 @forelse($carros as $carro)
-<<<<<<< HEAD
-     <p>Nome => {{$carro->nome}} placa => ({{$carro->placa}}) {!!HTML::link("carros/editar/{$carro->id}", 'Editar')!!}</p>
-=======
-     <p>Nome => {{$carro->nome}} placa => ({{$carro->placa}})</p>
->>>>>>> origin/master
-      
 
+     <p>Nome => {{$carro->nome}} placa => ({{$carro->placa}}) {!!HTML::link("carros/editar/{$carro->id}", 'Editar')!!} |{!!HTML::link("carros/deletar/{$carro->id}", 'Deletar')!!}</p>
+
+     <p>Nome => {{$carro->nome}} placa => ({{$carro->placa}})</p>
 @empty
     <p>Nenhum carro cadastrado</p>
 @endforelse
