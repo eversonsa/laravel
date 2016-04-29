27d125a1a0537eabdd31aaa68b560f5f2404ac51
everson <everson@everson-PC>
2016-04-29 00:13:40 -0300

    Criado novas tabelas via migration, Criado novos modelos

--- app/Http/Controllers/CarrosController.php
+++ app/Http/Controllers/CarrosController.php
@@ -6,55 +6,67 @@ use App\Models\Carro;
 use Illuminate\Http\Request;
 use Validator;
 
-class CarrosController extends Controller
-{
-    public function getIndex(){
-        $carros = Carro::paginate(2);
+class CarrosController extends Controller {
+
+    public function getIndex() {
+        $carros = Carro::paginate(7);
         return view('carros.index', compact('carros'));
     }
-    public function getAdicionar(){
+
+    public function getAdicionar() {
         return view('carros.create-edit');
     }
+
     public function postAdicionar(Request $request) {
+
         $dadosFormulario = $request->except('file');
-        
+
         $validar = Validator::make($dadosFormulario, Carro::$rules);
-            
-            $file = $request->file('file');
-            
-            if ( $request->hasFile('file') && $file->isValid()){
-                $file->move('assets/uploads/images', $file->getClientOriginalName());
-            }
-        
+        if ($validar->fails()) {
+            return redirect('carros/adicionar')
+                            ->withErrors($validar)
+                            ->withInput();
+        }
+        $file = $request->file('file');
+
+        if ($request->hasFile('file') && $file->isValid()) {
+            $file->move('assets/uploads/images', $file->getClientOriginalName());
+        }
+
         Carro::create($dadosFormulario);
+
         return redirect('carros');
     }
+
     public function getEditar($idCarro) {
         $carro = Carro::find($idCarro);
-        
+
         return view('carros.create-edit', compact('carro'));
     }
+
     public function postEditar(Request $request, $idcarro) {
-        $dadosFormulario = $request->except('_token');
-        
-         $validar = Validator::make($dadosFormulario, Carro::$rules);
-         if( $validar->fails() ){
-                return redirect("carros/editar/$idcarro")
-                      ->withErrors($validar)
-                      ->withInput();
-            }
-        Carro::where('id', $idcarro)->update($dadosFormulario);
-        
+        $dadosForm = $request->except('_token');
+
+        $validar = Validator::make($dadosForm, Carro::$rules);
+        if ($validar->fails()) {
+            return redirect("carros/editar/$idcarro")
+                            ->withErrors($validar)
+                            ->withInput();
+        }
+        Carro::where('id', $idcarro)->update($dadosForm);
+
         return redirect('carros/index');
     }
+
     public function getDeletar($idCarro) {
-        
+
         $carro = Carro::find($idCarro);
         $carro->delete();
         return redirect('carros/index');
     }
-    
+
     public function missingMethod($param = array()) {
         return "erro 404 pagina nao encontrada";
     }
+
 }
--- resources/views/carros/create-edit.blade.php
+++ resources/views/carros/create-edit.blade.php
@@ -6,12 +6,12 @@
     @endforeach
 @endif
 @if(isset($carro) )
-     <p>{!!Form::open(['url' => "carros/editar/$carro->id", 'files' => true]) !!}</p>
+     <p>{!!Form::open(['url' => "carros/editar/$carro->id", 'files' => true] ) !!}</p>
 @else
-    {!!Form::open(['url' => 'carros/adicionar', 'files' => true])!!}
+    {!!Form::open(['url' => 'carros/adicionar', 'files' => true] ) !!}
 @endif     
-        {!!Form::text('nome', isset($carro->nome) ? $carro->nome : null, ['placeholder' => 'nome do carro'])!!}
-        {!!Form::text('placa', isset($carro->placa) ? $carro->placa : null, ['placeholder' => 'placa do carro'])!!}
+        {!! Form::text('nome', isset($carro->nome) ? $carro->nome : null, ['placeholder' => 'nome do carro'] ) !!}
+        {!! Form::text('placa', isset($carro->placa) ? $carro->placa : null, ['placeholder' => 'placa do carro'] ) !!}
         {!! Form::file('file') !!}
         <p> {!!Form::submit('Enviar')!!}</p>
 {!!Form::close()!!}
